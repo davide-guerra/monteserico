79f47ce056538fbc27746c81cc273aa8c0e87435
Davide Guerra <davide.guerra@yahoo.it>
2022-08-03 10:05:41 +0200

    Nuovo header 10

--- header.php
+++ header.php
@@ -50,32 +50,6 @@ global $post;
         'bg-transparent': !isFixed
         }"
         >
-        <div class="w-full bg-verde py-2 shadow-lg" :class="{ 'hidden': isFixed, 'hidden lg:block': !isFixed }">
-            <div class="flex flex-wrap max-w-7xl mx-auto px-4 justify-between">
-                <div class="text-white flex flex-column items-center gap-2">
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                    <a href="<?php echo( get_field('link_indirizzo', 8) ); ?>" target="_blank" class="cursor-pointer hover:opacity-90"><?php echo( get_field('indirizzo_breve', 8) ); ?></a>
-                </div>
-                <div class="flex flex-column items-center gap-6">
-                    <div class="text-white flex flex-column items-center gap-2">
-                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-                        <a href="tel:<?php echo( str_replace(' ', '', get_field('telefono', 8)) ); ?>" class="cursor-pointer hover:opacity-90"><?php echo( get_field('telefono', 8) ); ?></a>
-                    </div>
-                    <div class="text-white flex flex-column items-center gap-2">
-                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                        <a href="mailto:<?php echo( get_field('email', 8) ); ?>" class="cursor-pointer hover:opacity-90"><?php echo( get_field('email', 8) ); ?></a>
-                    </div>
-                    <div id="language-switcher-wrapper" class="text-white no-underline flex flex-row gap-6 items-center ml-8">
-                         <?php if( apply_filters( 'wpml_element_has_translations', NULL, $post->ID, 'page' ) ) { ?>
-                            <!--<div class="hidden lg:block">|</div>-->
-                        <?php } ?>
-                        <span class="hover:opacity-90 cursor-pointer">
-                            <?php do_action('wpml_add_language_selector'); ?>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="flex items-center flex-wrap max-w-7xl mx-auto px-4">
             <!-- First item: logo -->
             <div class="text-white order-2 lg:order-1 flex-1 flex justify-center lg:justify-start">
