19a00463da638cf66fc9ea93522b4c8052a322bf
Davide Guerra <davide.guerra@yahoo.it>
2022-08-03 16:13:31 +0200

    Header 4

--- header.php
+++ header.php
@@ -75,7 +75,7 @@ global $post;
                 <div id="hamburger-and-menu" class="order-1 h-full grow basis-0" @click.away="isOpen = false">
                     <div class="text-white hidden lg:flex flex-column items-center gap-2">
                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                    <a href="<?php echo( get_field('link_indirizzo', 8) ); ?>" target="_blank" class="cursor-pointer hover:opacity-90"><?php echo( get_field('indirizzo_breve', 8) ); ?></a>
+                        <a href="<?php echo( get_field('link_indirizzo', 8) ); ?>" target="_blank" class="cursor-pointer hover:opacity-90"><?php echo( get_field('indirizzo_breve', 8) ); ?></a>
                     </div>
                     <button @click="isOpen = !isOpen" type="button" class="block lg:hidden px-2 hover:opacity-90 focus:outline-none">
                         <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -126,9 +126,9 @@ global $post;
                         <a href="mailto:<?php echo( get_field('email', 8) ); ?>" class="cursor-pointer hover:opacity-90"><?php echo( get_field('email', 8) ); ?></a>
                     </div>
                     <?php if( apply_filters( 'wpml_element_has_translations', NULL, $post->ID, 'page' ) ) { ?>
-                        <div class="hidden lg:block">|</div>
+                        <div class="text-white hidden lg:block">|</div>
                     <?php } ?>
-                    <span class="hover:opacity-90 cursor-pointer">
+                    <span class="text-wihite hover:opacity-90 cursor-pointer">
                     <?php do_action('wpml_add_language_selector'); ?>
                     </span>
                 </div>
