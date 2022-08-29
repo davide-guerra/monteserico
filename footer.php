620b150207745f32b658a4bcaba74f10d0718a5e
Davide Guerra <davide.guerra@yahoo.it>
2022-08-29 08:47:49 +0200

    Link for footer icons - 2

--- footer.php
+++ footer.php
@@ -50,14 +50,18 @@ $home_id = get_option('page_on_front');
                     <span class="text-sm text-center lg:text-left"><?php echo( get_field('indirizzo', $home_id) ); ?></span>
                 </div>
             </a>
-            <div class="text-white flex flex-col items-center gap-2">
-                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-                <a href="tel:<?php echo( str_replace(' ', '', get_field('telefono', $home_id)) ); ?>" class="cursor-pointer hover:opacity-90 text-sm text-center lg:text-left"><?php echo( get_field('telefono', $home_id) ); ?></a>
-            </div>
-            <div class="text-white flex flex-col items-center gap-2">
-                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                <a href="mailto:<?php echo( get_field('email', $home_id) ); ?>" class="cursor-pointer hover:opacity-90 text-sm text-center lg:text-left"><?php echo( get_field('email', $home_id) ); ?></a>
-            </div>
+            <a href="tel:<?php echo( str_replace(' ', '', get_field('telefono', $home_id)) ); ?>" class="cursor-pointer text-white hover:opacity-90">
+                <div class="flex flex-col items-center gap-2">
+                    <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
+                    <span class="text-sm text-center lg:text-left"><?php echo( get_field('telefono', $home_id) ); ?></span>
+                </div>
+            </a>
+            <a href="mailto:<?php echo( get_field('email', $home_id) ); ?>" class="cursor-pointer text-white hover:opacity-90">
+                <div class="flex flex-col items-center gap-2">
+                    <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
+                    <span class="text-sm text-center lg:text-left"><?php echo( get_field('email', $home_id) ); ?></span>
+                </div>
+            </a>
         </div>
     </div>
     <div class="block text-center lg:flex lg:justify-between max-w-7xl mx-auto py-4 border-t border-white text-white text-xs mt-4">
