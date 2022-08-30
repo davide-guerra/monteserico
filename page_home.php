54019418931c33cb8ec78009d2ea17d65cde9a26
Davide Guerra <davide.guerra@yahoo.it>
2022-08-30 17:08:43 +0200

    Creazione fascia home - 6

--- page_home.php
+++ page_home.php
@@ -39,7 +39,6 @@ $page_id = $post->ID;
                 <div>
                   <a href="<?php echo('/' . $meta['slug']); ?>" class="mt-4 text-white text-xl hover:opacity-90 inline-block border border-white px-4 py-2">
                   <div class="flex flex-row gap-2 items-center">
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
                     <span><?php echo($meta['button']); ?></span>
                   </div>
                   </a>
