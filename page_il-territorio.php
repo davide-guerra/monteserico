7b1519b7fc2d1657a290a2173a591e1b7a49e88d
Davide Guerra <davide.guerra@yahoo.it>
2022-09-01 09:11:36 +0200

    Il territorio

--- page_il-territorio.php
+++ page_il-territorio.php
@@ -86,7 +86,7 @@ $page_id = $post->ID;
             </button>
       </div>
     </div>
-    <div class="relative lg:absolute lg:left-4 lg:top-4 lg:w-[20vw]">
+    <div class="relative px-4 lg:px-0 lg:absolute lg:left-4 lg:top-4 lg:w-[20vw]">
       <?php echo( get_field('testo_slider') ); ?>
     </div>
   </div>
