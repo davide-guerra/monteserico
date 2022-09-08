7d6148bbefc7f6ee15cea0b16c5d68a8b072c51b
Davide Guerra <davide.guerra@yahoo.it>
2022-09-08 11:40:24 +0200

    modifiche

--- page_contatti.php
+++ page_contatti.php
@@ -11,7 +11,7 @@ $page_id = $post->ID;
 <section class="w-full">
     <div class="w-full relative aspect-[16/9] md:aspect-[7/3] 2xl:aspect-[7/2]">
         <img src="<?php echo get_the_post_thumbnail_url($page_id, 'full'); ?>" class="w-full h-full object-cover brightness-75">
-        <div class="absolute bottom-0 left-1/2 -translate-x-2/4 z-10 mb-8">
+        <div class="absolute bottom-0 left-1/2 -translate-x-2/4 z-[8] mb-8">
             <h2 class="font-serif text-center text-white text-4xl lg:text-6xl"><?php the_title(); ?></h2>
             <!-- Show bradcrumbs -->
             <nav class="text-center text-white mt-4">
