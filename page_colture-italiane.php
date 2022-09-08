bc225e9d931d8306bd71d26bc1b16d49013ff401
Davide Guerra <davide.guerra@yahoo.it>
2022-09-08 15:23:19 +0200

    modifiche

--- page_colture-italiane.php
+++ page_colture-italiane.php
@@ -127,12 +127,12 @@ splide.mount();
         <img class="w-full" src="<?php echo( get_field('immagine_piccola_fascia_3') ); ?>">
       </div>
       <div class="mt-4 lg:mt-0 flex flex-col gap-4">
-        <p class="mt0 lg:mt-4 ml-0 lg:ml-8"><?php echo( get_field('testo_fascia_3') ); ?></p>
+        <p class="mt0 lg:mt-8 ml-0 lg:ml-8"><?php echo( get_field('testo_fascia_3') ); ?></p>
       </div>
     </div>
     <div class="w-full lg:grid lg:grid-cols-2 gap-0">
         <div class="mt-4 lg:mt-0 flex flex-col gap-4">
-            <p class="mt-0 lg:mt-4 mr-0 lg:mr-8"><?php echo( get_field('testo_fascia_4') ); ?></p>
+            <p class="mt-0 lg:mt-8 mr-0 lg:mr-8"><?php echo( get_field('testo_fascia_4') ); ?></p>
       </div>
       <div class="flex flex-col gap-4">
         <img class="w-full" src="<?php echo( get_field('immagine_piccola_fascia_4') ); ?>">
