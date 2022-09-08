4e4e547bd20aa47a275827d5092eb6cc638c518b
Davide Guerra <davide.guerra@yahoo.it>
2022-09-08 11:37:01 +0200

    modifiche

--- page_contatti.php
+++ page_contatti.php
@@ -11,10 +11,10 @@ $page_id = $post->ID;
 <section class="w-full">
     <div class="w-full relative aspect-[16/9] md:aspect-[7/3] 2xl:aspect-[7/2]">
         <img src="<?php echo get_the_post_thumbnail_url($page_id, 'full'); ?>" class="w-full h-full object-cover brightness-75">
-        <div class="absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 flex flex-col gap-4 z-10">
-            <h2 class="text-center text-white text-2xl lg:text-4xl"><?php the_title(); ?></h2>
+        <div class="absolute bottom-0 left-1/2 -translate-x-2/4 z-10 mb-8">
+            <h2 class="font-serif text-center text-white text-2xl lg:text-6xl"><?php the_title(); ?></h2>
             <!-- Show bradcrumbs -->
-            <nav class="text-center text-white">
+            <nav class="text-center text-white mt-4">
               <a href="<?php echo( (ICL_LANGUAGE_CODE == 'it') ? '/' : '/en' ); ?>" class="hover:opacity-90">Home</a> >
                 <?php
                     foreach (get_post_ancestors($page_id) as $ancestor) {
