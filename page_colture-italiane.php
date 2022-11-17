74c5167c2fec249f878e356713caf961c93e057e
Davide Guerra <davide@pop-os.localdomain>
2022-11-17 11:33:40 +0100

    modifiche colture italiane

--- page_colture-italiane.php
+++ page_colture-italiane.php
@@ -136,7 +136,7 @@ splide.mount();
       </div>
       <div class="mt-4 lg:mt-0 flex flex-col gap-4">
         <?php if ( !empty( get_field('titolo_fascia_3') ) ) { ?>
-        <h2 class="font-serif text-oro text-5xl font-bold"><?php echo( get_field('titolo_fascia_3') ); ?></h2>
+        <h2 class="font-serif text-oro text-5xl font-bold ml-0 lg:ml-8"><?php echo( get_field('titolo_fascia_3') ); ?></h2>
         <?php } ?>
         <p class="mt0 ml-0 lg:ml-8"><?php echo( get_field('testo_fascia_3') ); ?></p>
       </div>
